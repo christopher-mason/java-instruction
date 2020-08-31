3b57ccd5e25f7d693fdd54b3bb9d49c57f112084
Chris Mason <m1achris43@gmail.com>
2020-08-31 18:25:27 -0400

    init commit

---
+++ ch01_Test/src/TestApp.java
@@ -0,0 +1,11 @@
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		System.out.println("Success!");
+		System.out.println("Success!");
+		System.out.println("Success!");
+
+	}
+
+}
