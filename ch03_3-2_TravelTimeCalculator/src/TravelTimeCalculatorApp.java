b301bc0784ab688fa9b6d214f8a1f0912e184da9
Chris Mason <m1achris43@gmail.com>
2020-09-07 22:02:09 -0400

    init commit

---
+++ ch03_3-2_TravelTimeCalculator/src/TravelTimeCalculatorApp.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class TravelTimeCalculatorApp {
+
+	public static void main(String[] args) {
+		// Welcome
+		System.out.println("Welcome to the Travel time Calculator");
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		// Get user Input
+		
+		
+		// Business Rules
+		
+		
+		// Get user output
+		
+		
+		// Bye
+		System.out.println("Bye");
+
+	}
+
+}
