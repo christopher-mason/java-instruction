cd97718f3482e073ccf94d0fe77e853a698cf002
Chris Mason <m1achris43@gmail.com>
2020-09-12 10:58:00 -0400

    Chapter 4 Practice

--- ch04_ins_DoWhile/src/DoWhileApp.java
+++ ch04_ins_DoWhile/src/DoWhileApp.java
@@ -9,7 +9,7 @@ public class DoWhileApp {
 		do {
 			System.out.print("Say the magic word: ");
 			word = sc.next();
-		} while (!word.equalsIgnoreCase("please")); // note the exclamation point
+		} while (!word.equalsIgnoreCase("please")); // note the exclamation point. the while statement is saying 'do' while word is NOT please
 			
 		System.out.println("Thank you");
 
---
+++ ch04_ins_DoWhile/src/ForLoopApp.java
@@ -0,0 +1,22 @@
+
+public class ForLoopApp {
+
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		for (int i = 8; i > 0; i -=2) {
+			System.out.println("In loop with i = " + i + " and sum = " + sum);
+			sum +=i;
+		}
+		System.out.println("sum = " + sum);
+		
+		int factorial = 1;
+		
+		for (int i = 1; i <= 5; ++i) {
+			factorial = factorial * i;
+		}
+		
+		System.out.println("factorial of 5 is " + factorial);
+	}
+
+}
