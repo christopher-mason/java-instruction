ef0f4257952cd19007e8803475f4779bb4c88ec4
Chris Mason <m1achris43@gmail.com>
2020-09-20 22:14:25 -0400

    Homework

--- proj_7-1_ContactList/src/ContactListApp.java
+++ proj_7-1_ContactList/src/ContactListApp.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
 
 public class ContactListApp {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to the Contact List application");
+		Scanner sc = new Scanner(System.in);
 		
+		String firstName;
+		String lastName;
+		String email;
+		String phoneNumber;
+		
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) {
+			System.out.println("Enter First Name: ");
+			firstName = sc.next();
+			System.out.println("Enter Last Name: ");
+			lastName = sc.next();
+			System.out.println("Enter Email: ");
+			email = sc.next();
+			System.out.println("Enter Phone: ");
+			phoneNumber = sc.next();
+			
+		}
 
 	}
 
