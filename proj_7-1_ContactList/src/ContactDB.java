1370ac4a7227f999cd8a4e7a6bfe43e7a2668bb0
Chris Mason <m1achris43@gmail.com>
2020-09-21 18:08:10 -0400

    Chapter 7 Homework

--- proj_7-1_ContactList/src/ContactDB.java
+++ proj_7-1_ContactList/src/ContactDB.java
@@ -1,7 +1,57 @@
 
 public class ContactDB {
 	
-	public static String getContact(String contactList);
+	private String firstName;
+	private String lastName;
+	private String email;
+	private int phoneNumber;
+
+	public ContactDB() {
+		firstName = "";
+		lastName = "";
+		email = "";
+		phoneNumber = 0;
+	}
+
+	public ContactDB(String firstName, String lastName, String email, int phoneNumber) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+
+	}
+
+	public void setfirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getfirstName() {
+		return firstName;
+	}
+
+	public void setlastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getlastName() {
+		return lastName;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public double getPhoneNumber() {
+		return phoneNumber;
+	}
 	
 	
 	
--- proj_7-1_ContactList/src/ContactListApp.java
+++ proj_7-1_ContactList/src/ContactListApp.java
@@ -3,25 +3,36 @@ import java.util.Scanner;
 public class ContactListApp {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to the Contact List application");
-		Scanner sc = new Scanner(System.in);
-		
-		String firstName;
-		String lastName;
-		String email;
-		String phoneNumber;
-		
 		String choice = "y";
 		while (choice.equalsIgnoreCase("y")) {
-			System.out.println("Enter First Name: ");
-			firstName = sc.next();
-			System.out.println("Enter Last Name: ");
-			lastName = sc.next();
-			System.out.println("Enter Email: ");
-			email = sc.next();
-			System.out.println("Enter Phone: ");
-			phoneNumber = sc.next();
-			
+			// get the input from the user
+			String firstName = Console.getString("enter first name");
+			String lastName = Console.getString("enter last name");
+			String email = Console.getString("enter email ");
+//			int phoneNumber = Console.getInt("phoneNumber:     ", 0, 100);
+
+			// test without object params
+			ContactDB contactList = new ContactDB();
+			contactList.setfirstName(firstName);
+			contactList.setlastName(lastName);
+			contactList.setEmail(email);
+//			contactList.setPhoneNumber(phoneNumber);
+
+			// test with object params
+			ContactDB contactListConstructor = new ContactDB(firstName, lastName, email, 0);
+			contactListConstructor.setfirstName(firstName);
+			contactListConstructor.setlastName(lastName);
+			contactListConstructor.setEmail(email);
+
+			System.out.println(contactList.getfirstName());
+			System.out.println(contactList.getlastName());
+			System.out.println(contactList.getEmail());
+
+			System.out.println(contactListConstructor.getfirstName());
+			System.out.println(contactListConstructor.getlastName());
+			System.out.println(contactListConstructor.getEmail());
+//			System.out.println(contactList.getPhoneNumber());
+
 		}
 
 	}
