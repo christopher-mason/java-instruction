b3e8f7c6c58d74604b2bc6770ef4d3437cfa9cb3
Chris Mason <m1achris43@gmail.com>
2020-09-24 22:21:20 -0400

    Work

--- proj_4-2_FactorialCalculator/src/FactorialCalculatorApp.java
+++ proj_4-2_FactorialCalculator/src/FactorialCalculatorApp.java
@@ -1,9 +1,29 @@
+import java.util.Scanner;
 
 public class FactorialCalculatorApp {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to the Factorial Calculator");
 		
-
+		Scanner sc = new Scanner(System.in);
+		
+		int number = 0;
+		long factorial = 1;
+		
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) {
+			System.out.print("Enter an integer that is greater than zero but less than 10: ");
+			number = sc.nextInt();
+			for (int i = number; i <= 10; i++) {
+				factorial = factorial * i;
+			} if (number > 10) {
+				System.out.println("Error! Invalid integer.");
+			}
+			System.out.println("The factorial of " + number + " is " + factorial);
+			System.out.println("Continue? (y/n)");
+			choice = sc.next();
+		}
+		System.out.println("Bye");
 	}
 
 }
