74d16a452d9e202e032f2c798c72fc1a1dc2592d
Chris Mason <m1achris43@gmail.com>
2020-09-24 22:56:05 -0400

    Work

--- proj_4-4_CommonDivisorCalculator/src/CommonDivisorCalculatorApp.java
+++ proj_4-4_CommonDivisorCalculator/src/CommonDivisorCalculatorApp.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 
 public class CommonDivisorCalculatorApp {
 
 	public static void main(String[] args) {
+		System.out.println("Greatest Common Divisor Finder");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
 		
 
 	}
