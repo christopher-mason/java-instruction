36b4d71bb2eabb759547cb5d56a3163591db2380
Chris Mason <m1achris43@gmail.com>
2020-09-24 21:36:02 -0400

    More Work

--- proj_4-1_TableOfPowers/src/TableOfPowersApp.java
+++ proj_4-1_TableOfPowers/src/TableOfPowersApp.java
@@ -1,9 +1,32 @@
+import java.util.Scanner;
 
 public class TableOfPowersApp {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to the Squares and Cubes Table");
 		
+		Scanner sc = new Scanner(System.in);
+		
+		int i;
 
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) {
+			System.out.print("Enter integer: ");
+			i = sc.nextInt();
+			double squared = 0;
+			double cubed = 0;
+			for (int p = 0; p < i; p++) {
+				squared = Math.pow(p, 2);
+				cubed = Math.pow(p, 3);
+			}
+			System.out.println("Number     Squared     Cubed" + "\n" + 
+							   "==============================");
+			System.out.println(i + "          " + squared + "         " +cubed);
+			System.out.println("Continue? (y/n)");
+			choice = sc.next();
+			
+		}
+		System.out.println("Bye");
 	}
 
 }
