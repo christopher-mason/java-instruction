76d7a98b608490832f33b258001b398dec2530c2
Chris Mason <m1achris43@gmail.com>
2020-09-26 08:02:31 -0400

    Chapter 8-1 Project

--- proj_8-1_NumberChecker/src/MyConsole.java
+++ proj_8-1_NumberChecker/src/MyConsole.java
@@ -1,4 +1,22 @@
 
-public class MyConsole {
+public class MyConsole extends Console{
+	
+	@Override
+	public String getString(String prompt) {
+		String userInput = "";
+
+		while (userInput.equals("")) {
+			System.out.print(prompt);
+			userInput = sc.nextLine();
+
+			if (userInput.equals("")) {
+				System.out.println("Error! This entry is required. Try again.");
+			}
+		}
+
+		System.err.println("Leaving getString with " + userInput);
+
+		return userInput;
+	}
 
 }
