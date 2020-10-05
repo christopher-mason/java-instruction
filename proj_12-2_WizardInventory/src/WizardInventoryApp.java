6f22a7752d8e55947136e05da3caf82ebc93409d
Chris Mason <m1achris43@gmail.com>
2020-10-05 00:08:19 -0400

    Chapter 12 Homework

--- proj_12-2_WizardInventory/src/WizardInventoryApp.java
+++ proj_12-2_WizardInventory/src/WizardInventoryApp.java
@@ -1,9 +1,37 @@
+import java.util.ArrayList;
 
 public class WizardInventoryApp {
 
 	public static void main(String[] args) {
+		System.out.println("Wizard Inventory Game");
 		
+		ArrayList<String> inventory = new ArrayList<>();
+		inventory.add("wooden staff");
+		inventory.add("wizard hat");
+		inventory.add("cloth shoes");
+		
+		String choice = "";
+		
+		while (!choice.equalsIgnoreCase("exit")) {
+			displayMenu();
+			choice = Console.getString("Command: ");
+			if (choice.equals("show")) {
+				System.out.println(inventory);
+			} else if (choice.equals("grab")) {
+				System.out.println("Name: " + Console.getString(""));
+				
+			}
+		}
 
+	}
+	
+	public static void displayMenu() {
+		System.out.println("COMMAND MENU");
+		System.out.println("show - Show all items");
+		System.out.println("grab - Grab an item");
+		System.out.println("edit - Edit an item");
+		System.out.println("drop - Drop an item");
+		System.out.println("exit - Exit program");
 	}
 
 }
