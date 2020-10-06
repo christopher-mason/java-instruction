3f1baf523dd32dbabea4d2f2b844c93d6ebcc83c
Chris Mason <m1achris43@gmail.com>
2020-10-05 21:28:39 -0400

    Chapter 13 Stuff

--- ch13_Instruction/src/mason/app/StringApp.java
+++ ch13_Instruction/src/mason/app/StringApp.java
@@ -7,7 +7,7 @@ public class StringApp {
 		//Length()
 		String productCode = "java";
 		System.out.println("Length of " + productCode + " is " + productCode.length());
-		
+		System.out.println();
 		// Index
 		String name = "Martin Van Buren";
 		int firstSpace = name.indexOf(" ");
@@ -16,9 +16,35 @@ public class StringApp {
 		System.out.println("Found second space at index " + secondSpace);
 		int thirdSpace = name.indexOf(" ", secondSpace + 1);
 		System.out.println("Found third space at index " + thirdSpace);
+		System.out.println();
 		
 		char fifthChar = name.charAt(4);
 		System.out.println("The fifth character is: " + fifthChar);
+		System.out.println();
+		
+		String strWithSpaces = "   a  b c ";
+		String trimmed = strWithSpaces.trim();
+		System.out.println("Trimmed= [" + trimmed + "]");
+		System.out.println();
+		
+		String fullName = "Mike Murach";
+		int spaceIndex = fullName.indexOf(" ");
+		String firstName = fullName.substring(0, spaceIndex);
+		String lastName = fullName.substring(spaceIndex + 1);
+		System.out.println("firstName = [" + firstName + "]\nlastName = [" + lastName + "]");
+		System.out.println();
+		
+		String phoneNumber = "513-254-7694";
+		String dotPhoneNumber = phoneNumber.replace("-", ".");
+		System.out.println("dotPhoneNumber = " + dotPhoneNumber);
+		System.out.println();
+		
+		String firstNames = "Dave Chris Christina";
+		String[] firstNamesArray = firstNames.split(" ");
+		System.out.println("firstNamesArray length = " + firstNamesArray.length);
+		for (String f : firstNamesArray) {
+			System.out.println("[" + f + "]");
+		}
 	
 	}
 
