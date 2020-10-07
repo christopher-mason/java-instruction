d602cb7ca764c1fd07f8b8f41a20f092dcd22ad9
Chris Mason <m1achris43@gmail.com>
2020-10-06 22:22:07 -0400

    Chapter 13 Proj 13-1

--- proj_13-1_HTMLConverter/src/mason/app/HTMLConverterApp.java
+++ proj_13-1_HTMLConverter/src/mason/app/HTMLConverterApp.java
@@ -12,6 +12,23 @@ public class HTMLConverterApp {
 					"	<li>Butter</li>\n" +
 					"</ul>";
 		
+		System.out.println("INPUT");
+		System.out.println(html);
+		System.out.println();
+		
+		String html1 = "<h1>Grocery List<h1>\n";
+		String html2 = "<ul>\n" + "<li>Eggs</li>\n" + "<li>Milk</li>\n" + "<li>Butter</li>\n" + "</ul>";
+
+		System.out.println("OUTPUT");
+
+		html1 = html1.replace("<h1>", "");
+		html2 = html2.replace("<ul>", "");
+		html2 = html2.replace("<li>", "* ");
+		html2 = html2.replace("</li>", "");
+		html2 = html2.replace("</ul>", "");
+		html2 = html2.trim();
+
+		System.out.println(html1 + html2);
 		
 		
 	}
