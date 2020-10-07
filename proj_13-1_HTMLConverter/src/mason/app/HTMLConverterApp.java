1cbc75ae1f99894296cdd3a6feddd3376fe140b1
Chris Mason <m1achris43@gmail.com>
2020-10-06 22:12:09 -0400

    init commit

---
+++ proj_13-1_HTMLConverter/src/mason/app/HTMLConverterApp.java
@@ -0,0 +1,19 @@
+package mason.app;
+
+public class HTMLConverterApp {
+
+	public static void main(String[] args) {
+		System.out.println("HTML Converter");
+		
+		String html = "<h1>Grocery List</h1>\n" +
+					"<u1\n" +
+					"	<li>Eggs</li>\n" +
+					"	<li>Milk</li>\n" +
+					"	<li>Butter</li>\n" +
+					"</ul>";
+		
+		
+		
+	}
+
+}
