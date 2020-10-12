d4e7eddac4eefb580ca755e52386adba82e15d07
Chris Mason <m1achris43@gmail.com>
2020-10-11 20:18:04 -0400

    Chapter 14 Proj 1

--- proj_14-1_ReservationCalculator/src/mason/app/ReservationCalculatorApp.java
+++ proj_14-1_ReservationCalculator/src/mason/app/ReservationCalculatorApp.java
@@ -3,8 +3,19 @@ package mason.app;
 public class ReservationCalculatorApp {
 
 	public static void main(String[] args) {
+		System.out.println("Reservation Calculator");
 		
+		String choice = "y";
+		
+		while (choice.equalsIgnoreCase("y")) {
+			int arMon = Console.getInt("Enter the arrival month (1-12): ");
+			int arDay = Console.getInt("Enter the arrival day (1-31): ");
+			int arYear = Console.getInt("Enter the arrival year: ");
+			System.out.println(arMon + arDay + arYear);
+		}
 
+		choice = Console.getString("Continue? (y/n): ");
+		System.out.println("Bye!");
 	}
 
 }
