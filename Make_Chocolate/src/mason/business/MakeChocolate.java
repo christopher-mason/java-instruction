e5d2a9c28dd27731f908199f98a77491509e62b6
Chris Mason <m1achris43@gmail.com>
2020-11-25 23:18:42 -0500

    Edited Chocolate App

--- Make_Chocolate/src/mason/business/MakeChocolate.java
+++ Make_Chocolate/src/mason/business/MakeChocolate.java
@@ -2,43 +2,15 @@ package mason.business;
 
 public class MakeChocolate {
 	
-	private int small;
-	private int big;
-	private int goal;
-	
-	public MakeChocolate() {
-		super();
-	}
-
-	public MakeChocolate(int small, int big, int goal) {
-		super();
-		this.small = small;
-		this.big = big;
-		this.goal = goal;
-	}
-
-	public int getSmall() {
-		return small;
-	}
-
-	public void setSmall(int small) {
-		this.small = small;
-	}
-
-	public int getBig() {
-		return big;
-	}
-
-	public void setBig(int big) {
-		this.big = big;
-	}
-
-	public int getGoal() {
-		return goal;
-	}
-
-	public void setGoal(int goal) {
-		this.goal = goal;
+	public static int makeChocolate(int small, int big, int goal) {
+		int result = goal % 5;
+		
+		if (small + big * 5 < goal) {
+			return -1;
+		} else if (small + big * 5 == goal) {
+			return small;
+		} 
+		 return result;
 	}
 	
 	
--- Make_Chocolate/src/mason/web/ChocolateApp.java
+++ Make_Chocolate/src/mason/web/ChocolateApp.java
@@ -1,16 +1,18 @@
 package mason.web;
 
+import java.util.Scanner;
+
 public class ChocolateApp {
 	
-	public static int makeChocolate(int small, int big, int goal) {
-		int result = goal % 5;
+	public static void main(String[] args) {
 		
-		if (small + big * 5 < goal) {
-			return -1;
-		} else if (small + big * 5 == goal) {
-			return small;
-		} 
-		 return result;
+		Scanner sc = new Scanner(System.in);
+		
+		String choice = "y";
+		while(choice.equalsIgnoreCase("y")) {
+			
+		}
+		
 	}
 
 }
