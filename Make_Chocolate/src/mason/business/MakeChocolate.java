06d6806a85c0993c128de8f147ac9ff53cdf4c53
Chris Mason <m1achris43@gmail.com>
2020-11-26 23:15:11 -0500

    Making Chocolate

--- Make_Chocolate/src/mason/business/MakeChocolate.java
+++ Make_Chocolate/src/mason/business/MakeChocolate.java
@@ -2,16 +2,7 @@ package mason.business;
 
 public class MakeChocolate {
 	
-	public static int makeChocolate(int small, int big, int goal) {
-		int result = goal % 5;
-		
-		if (small + big * 5 < goal) {
-			return -1;
-		} else if (small + big * 5 == goal) {
-			return small;
-		} 
-		 return result;
-	}
+	
 	
 	
 
--- Make_Chocolate/src/mason/web/ChocolateApp.java
+++ Make_Chocolate/src/mason/web/ChocolateApp.java
@@ -15,5 +15,16 @@ public class ChocolateApp {
 		}
 		
 	}
+	
+	public static int makeChocolate(int small, int big, int goal) {
+		int result = goal % 5;
+		
+		if (small + big * 5 < goal) {
+			return -1;
+		} else if (small + big * 5 == goal) {
+			return small;
+		} 
+		 return result;
+	}
 
 }
