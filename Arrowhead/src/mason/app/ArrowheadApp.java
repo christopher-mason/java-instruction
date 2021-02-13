b447ff2e06243f04a7241610fe36b0db2a4359be
Chris Mason <m1achris43@gmail.com>
2021-02-12 22:18:35 -0500

    Edited Arrowhead

--- Arrowhead/src/mason/app/ArrowheadApp.java
+++ Arrowhead/src/mason/app/ArrowheadApp.java
@@ -4,6 +4,8 @@ public class ArrowheadApp {
 
 	public static void main(String[] args) {
 		 System.out.println("Welcome to the Arrowhead App!!");
+		 
+		 // Prompt user how big they want the arrowhead, by number of lines
 
 	}
 
