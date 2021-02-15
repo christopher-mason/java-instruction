4d1378b14c39a853585658fd43e66eb090af0f2a
Chris Mason <m1achris43@gmail.com>
2021-02-14 22:50:09 -0500

    Added pseudocode

--- Arrowhead/src/mason/app/ArrowheadApp.java
+++ Arrowhead/src/mason/app/ArrowheadApp.java
@@ -5,7 +5,8 @@ public class ArrowheadApp {
 	public static void main(String[] args) {
 		 System.out.println("Welcome to the Arrowhead App!!");
 		 
-		 // Prompt user how big they want the arrowhead, by number of lines
+		 // Prompt user how big they want the arrowhead, by number of lines. Prompt the user to provide the size of an arrowhead.  
+		 // The app will print an arrowhead in the console, line by line, for the # of lines given by the user
 
 	}
 
